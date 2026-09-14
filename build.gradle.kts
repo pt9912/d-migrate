8c81b03c37eb68fc95863a38f61efb52dad542c6
pt9912 <66826472+pt9912@users.noreply.github.com>
2026-09-14 07:56:27 +0200

    Bump version to 1.6.0-SNAPSHOT for next development cycle

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ fun normalizedReleaseVersion(raw: String?): String? {
     return normalized.takeIf { semverLike.matches(it) }
 }
 
-val defaultProjectVersion = "1.5.1"
+val defaultProjectVersion = "1.6.0-SNAPSHOT"
 val resolvedProjectVersion =
     normalizedReleaseVersion(findProperty("releaseVersion")?.toString())
         ?: normalizedReleaseVersion(System.getenv("DMIGRATE_VERSION"))
